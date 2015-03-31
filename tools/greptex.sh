45ef7f0d0be5695d90db5edf52dd209e4c26038c
sharkdp <davidpeter@web.de>
2015-03-31 18:11:34 +0200

    grep through front end endmatter

--- tools/greptex.sh
+++ tools/greptex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep --color --ignore-case --line-number "$@" chapters/*.tex appendices/*.tex
+grep --color --ignore-case --line-number "$@" chapters/*.tex appendices/*.tex frontmatter/*.tex endmatter/*.tex
